757e7a9e3b32f6eca58fce84cdbac0330ac66140
Adrian <league711@wp.pl>
2022-06-01 20:08:26 +0200

    Test2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
 import sqlite3
 sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-main = input()
 cur= sqlite_con.cursor()
+
 
 cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
                   to_subscriber data_type INTEGER, 
@@ -11,12 +11,11 @@ cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER,
                   celltower data_type INTEGER);''') 
 
 with open('polaczenia_duze.csv','r') as fin: 
-    
-    reader = csv.reader(fin, delimiter = ";") 
-    next(reader, None) 
-    rows = [x for x in reader]
-    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
-    sqlite_con.commit()
+  reader = csv.reader(fin, delimiter = ";") 
+  next(reader, None) 
+  rows = [x for x in reader]
+  cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+  sqlite_con.commit()
 
 if __name__ == "__main__":
-    main()
+    pass
