6aeb451a740879cd2a7aa5813615a7772a2ad0c1
Adrian <league711@wp.pl>
2022-06-01 20:23:11 +0200

    Test5

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import csv
 import sqlite3
 
 def main():
-  sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-  cur= sqlite_con.cursor()
+  sqlite_connection = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+  cursor= sqlite_connection.cursor()
 
 
-  cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+  cursor.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
                   to_subscriber data_type INTEGER, 
                   datetime data_type timestamp, 
                   duration data_type INTEGER , 
                   celltower data_type INTEGER);''') 
 
-  with open('polaczenia_duze.csv','r') as fin: 
-    reader = csv.reader(fin, delimiter = ";") 
+  with open('polaczenia_duze.csv','r') as file: 
+    reader = csv.reader(file, delimiter = ";") 
     next(reader, None) 
     rows = [x for x in reader]
-    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
-    sqlite_con.commit()
-    print(cur.fetchall()[0])
+    cursor.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+    sqlite_connection.commit()
+    print(cursor.fetchall()[0])
 if __name__ == "__main__":
     main
