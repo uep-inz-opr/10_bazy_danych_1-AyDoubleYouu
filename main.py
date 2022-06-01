39d910fe327ac760336cb4e0540613fbe4ee0907
Adrian <102667006+AyDoubleYouu@users.noreply.github.com>
2022-06-01 21:10:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 import sqlite3
 
+polaczenia_duze = input()
+
 class ReportGenerator:
   def __init__(self,connection, escape_string = "(%s)"):
     self.connection = connection
@@ -9,31 +11,32 @@ class ReportGenerator:
 
   def generate_report(self):
     cursor = self.connection.cursor()
-    sql_query = f"Select sum(duration) from polaczenia where from_subscriber ={self.escape_string}"
+    sql_query = f"Select sum(duration) from polaczenia"
     cursor.execute(sql_query)
     result = cursor.fetchone()[0]
-    self.report_text = f"{result}"
+    self.report_text = result
 
   def get_report(self):	
     return self.report_text
        
 if __name__ == "__main__":
-    sqlite_connection = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    cursor= sqlite_connection.cursor()
 
-    cursor.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+    sqlite_connection = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    cur= sqlite_connection.cursor()
+
+    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
                   to_subscriber data_type INTEGER, 
                   datetime data_type timestamp, 
                   duration data_type INTEGER , 
                   celltower data_type INTEGER);''') 
 
-    with open('polaczenia_duze.csv','r') as file: 
+    with open(polaczenia_duze,'r') as file: 
         reader = csv.reader(file, delimiter = ";") 
         header = next(reader) 
         rows = [x for x in reader]
-        cursor.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
         sqlite_connection.commit()
 
         rg = ReportGenerator(sqlite_connection, escape_string="?")
         rg.generate_report()
-        print(rg.get_report())
+        rg.get_report()
